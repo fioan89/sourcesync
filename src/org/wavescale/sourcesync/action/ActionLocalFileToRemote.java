2ececc57d60cfcf8c07ab72d69b7988dcd9593ab
Faur Ioan-Aurel <fioan89@gmail.com>
2013-12-30 20:11:37 +0200

    Allow user to take advantage of the new logging mechanism and of course now you can sync the current selected file using scp protocol.

--- src/org/wavescale/sourcesync/action/ActionLocalFileToRemote.java
+++ src/org/wavescale/sourcesync/action/ActionLocalFileToRemote.java
@@ -1,19 +1,18 @@
 package org.wavescale.sourcesync.action;
 
-import com.intellij.notification.*;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.ui.popup.JBPopupFactory;
-import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.ui.awt.RelativePoint;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.wavescale.sourcesync.config.SCPConfiguration;
+import org.wavescale.sourcesync.factory.ConfigConnectionFactory;
 import org.wavescale.sourcesync.factory.ModuleConnectionConfig;
+import org.wavescale.sourcesync.logger.BalloonLogger;
+import org.wavescale.sourcesync.logger.EventDataLogger;
+import org.wavescale.sourcesync.synchronizer.SCPFileSynchronizer;
 
-import javax.xml.crypto.Data;
+import java.io.File;
 
 /**
  * ****************************************************************************
@@ -32,22 +31,27 @@ public class ActionLocalFileToRemote extends AnAction {
         // and get out
         Project currentProject = DataKeys.PROJECT.getData(e.getDataContext());
         String moduleName = currentProject.getName();
-        if (ModuleConnectionConfig.getInstance().getAssociationFor(moduleName) == null) {
+        String associationName = ModuleConnectionConfig.getInstance().getAssociationFor(moduleName);
+        if (associationName == null) {
             showNoConnectionSpecifiedError(e, moduleName);
+            return;
         }
+        VirtualFile virtualFile = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        SCPConfiguration connectionConfiguration = (SCPConfiguration) ConfigConnectionFactory.getInstance().getConnectionConfiguration(associationName);
+        SCPFileSynchronizer scpFileSynchronizer = new SCPFileSynchronizer(connectionConfiguration, e.getProject());
+        scpFileSynchronizer.connect();
+        File relativeFile = new File(virtualFile.getPath().replaceFirst(currentProject.getBasePath(), ""));
+        scpFileSynchronizer.syncFile(relativeFile.getPath(), relativeFile.getParent());
+        scpFileSynchronizer.disconnect();
     }
 
     private void showNoConnectionSpecifiedError(AnActionEvent e, String moduleName) {
-        StatusBar statusBar = WindowManager.getInstance().getStatusBar(DataKeys.PROJECT.getData(e.getDataContext()));
         StringBuilder message = new StringBuilder();
         message.append("There is no connection type associated to <b>").append(moduleName)
                 .append("</b> module.\nPlease right click on module name and then select <b>Module Connection Configuration</b> to select connection type!");
+        BalloonLogger.logBalloonError(message.toString(), e.getProject());
+        EventDataLogger.logError(message.toString(), e.getProject());
 
-        JBPopupFactory.getInstance().createHtmlTextBalloonBuilder(message.toString(), MessageType.ERROR, null).
-                setFadeoutTime(7500).createBalloon().show(RelativePoint.getCenterOf(statusBar.getComponent()),
-                Balloon.Position.atRight);
-        Notification notification = new Notification("SourceSync Notifications", "Sync error", message.toString(), NotificationType.ERROR);
-        Notifications.Bus.notify(notification, e.getProject());
 
     }
 }
