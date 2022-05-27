46f06ed5ae773307df3d9cef31c9a915ce7ff327
Ioan Faur <fioan89@gmail.com>
2022-05-27 19:12:12 +0300

    Remove usage of deprecated API
    - use java nio path operations to subtract base paths instead of string manipulation
    - remove UT's, they were pure crap

--- src/main/java/org/wavescale/sourcesync/action/ActionSelectedFilesToRemote.java
+++ src/main/java/org/wavescale/sourcesync/action/ActionSelectedFilesToRemote.java
@@ -1,13 +1,9 @@
 package org.wavescale.sourcesync.action;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 import org.jetbrains.annotations.NotNull;
 import org.wavescale.sourcesync.api.ConnectionConfiguration;
 import org.wavescale.sourcesync.api.FileSynchronizer;
@@ -17,11 +13,15 @@ import org.wavescale.sourcesync.factory.ConfigConnectionFactory;
 import org.wavescale.sourcesync.factory.ConnectionConfig;
 import org.wavescale.sourcesync.logger.BalloonLogger;
 import org.wavescale.sourcesync.logger.EventDataLogger;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Semaphore;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.project.ProjectKt;
 
 /**
  * ****************************************************************************
@@ -34,22 +34,26 @@ import java.util.concurrent.Semaphore;
  * For any issues or questions send an email at: fioan89@gmail.com              *
  * *****************************************************************************
  */
-public class ActionSelectedFilesToRemote extends AnAction {
-    public void actionPerformed(final AnActionEvent e) {
+public class ActionSelectedFilesToRemote extends AnAction
+{
+    public void actionPerformed(final AnActionEvent e)
+    {
         // first check if there's a connection type associated to this module.
         // If not alert the user and get out
         Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
 
         String projectName = project.getName();
         String associationName = ConnectionConfig.getInstance().getAssociationFor(projectName);
-        if (associationName == null) {
+        if (associationName == null)
+        {
             Utils.showNoConnectionSpecifiedError(projectName);
             return;
         }
 
         // get a list of selected virtual files
         VirtualFile[] virtualFiles = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
-        if (virtualFiles.length <= 0) {
+        if (virtualFiles.length <= 0)
+        {
             StringBuilder builder = new StringBuilder("Project <b>");
             builder.append(e.getProject().getName()).append("</b>! does not have files selected!");
             BalloonLogger.logBalloonInfo(builder.toString(), e.getProject());
@@ -59,26 +63,33 @@ public class ActionSelectedFilesToRemote extends AnAction {
 
         // start sync
         final ConnectionConfiguration connectionConfiguration = ConfigConnectionFactory.getInstance().
-                getConnectionConfiguration(associationName);
+            getConnectionConfiguration(associationName);
         final Semaphore semaphores = new Semaphore(connectionConfiguration.getSimultaneousJobs());
         final int allowed_sessions = virtualFiles.length <= connectionConfiguration.getSimultaneousJobs() ?
-                virtualFiles.length : connectionConfiguration.getSimultaneousJobs();
+            virtualFiles.length : connectionConfiguration.getSimultaneousJobs();
         final SynchronizationQueue synchronizationQueue = new SynchronizationQueue(e.getProject(), connectionConfiguration, allowed_sessions);
         synchronizationQueue.startCountingTo(virtualFiles.length);
         final BlockingQueue<FileSynchronizer> queue = synchronizationQueue.getSyncQueue();
-        for (final VirtualFile virtualFile : virtualFiles) {
-            if (virtualFile != null && new File(virtualFile.getPath()).isFile()) {
-                if (Utils.canBeUploaded(virtualFile.getName(), connectionConfiguration.getExcludedFiles())) {
-                    final Path uploadLocation = Utils.dirsToFileFromProjectRoot(virtualFile, project);
-                    ProgressManager.getInstance().run(new Task.Backgroundable(e.getProject(), "Uploading", false) {
+        for (final VirtualFile virtualFile : virtualFiles)
+        {
+            if (virtualFile != null && new File(virtualFile.getPath()).isFile())
+            {
+                if (Utils.canBeUploaded(virtualFile.getName(), connectionConfiguration.getExcludedFiles()))
+                {
+                    final Path uploadLocation = Utils.dirsToFileFromProjectRoot(virtualFile, ProjectKt.getStateStore(project));
+                    ProgressManager.getInstance().run(new Task.Backgroundable(e.getProject(), "Uploading", false)
+                    {
                         @Override
-                        public void run(@NotNull ProgressIndicator indicator) {
+                        public void run(@NotNull ProgressIndicator indicator)
+                        {
                             FileSynchronizer fileSynchronizer = null;
-                            try {
+                            try
+                            {
                                 semaphores.acquire();
                                 fileSynchronizer = queue.take();
                                 fileSynchronizer.setIndicator(indicator);
-                                if (fileSynchronizer != null) {
+                                if (fileSynchronizer != null)
+                                {
                                     fileSynchronizer.connect();
                                     // so final destination will look like this:
                                     // root_home/ + project_relative_path_to_file/
@@ -86,22 +97,32 @@ public class ActionSelectedFilesToRemote extends AnAction {
                                 fileSynchronizer.syncFile(virtualFile.getPath(), uploadLocation);
                                 queue.put(fileSynchronizer);
                                 synchronizationQueue.count();
-                            } catch (InterruptedException e1) {
+                            }
+                            catch (InterruptedException e1)
+                            {
                                 e1.printStackTrace();
-                            } finally {
+                            }
+                            finally
+                            {
                                 semaphores.release();
                             }
                         }
                     });
 
-                } else {
-                    if (virtualFile != null) {
+                }
+                else
+                {
+                    if (virtualFile != null)
+                    {
                         EventDataLogger.logWarning("File <b>" + virtualFile.getName() + "</b> is filtered out!", e.getProject());
                         synchronizationQueue.count();
                     }
                 }
-            } else {
-                if (virtualFile != null) {
+            }
+            else
+            {
+                if (virtualFile != null)
+                {
                     EventDataLogger.logWarning("File <b>" + virtualFile.getName() + "</b> is a directory!", e.getProject());
                     synchronizationQueue.count();
                 }
--- src/main/java/org/wavescale/sourcesync/api/Utils.java
+++ src/main/java/org/wavescale/sourcesync/api/Utils.java
@@ -1,13 +1,11 @@
 package org.wavescale.sourcesync.api;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.intellij.openapi.components.impl.stores.IProjectStore;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 
 /**
  * ****************************************************************************
@@ -20,7 +18,8 @@ import java.nio.file.Paths;
  * For any issues or questions send an email at: fioan89@gmail.com              *
  * *****************************************************************************
  */
-public class Utils {
+public class Utils
+{
 
     /**
      * Checks if a given filename can be uploaded or not.
@@ -31,16 +30,17 @@ public class Utils {
      *                           extension MUST contain the dot character - ex: ".crt .iml .etc"
      * @return <code>true</code> if file extension is not on the extensionsToFilter, <code>False</code> otherwise.
      */
-    public static boolean canBeUploaded(String fileName, String extensionsToFilter) {
+    public static boolean canBeUploaded(String fileName, String extensionsToFilter)
+    {
         String extension = ".";
 
-        if (fileName != null) {
+        if (fileName != null)
+        {
             int i = fileName.lastIndexOf('.');
-            if (i >= 0) {
+            if (i >= 0)
+            {
                 extension += fileName.substring(i + 1);
-                if (extensionsToFilter.contains(extension)) {
-                    return false;
-                }
+                return !extensionsToFilter.contains(extension);
             }
             return true;
         }
@@ -52,11 +52,12 @@ public class Utils {
      *
      * @param projectName module or project name.
      */
-    public static void showNoConnectionSpecifiedError(String projectName) {
+    public static void showNoConnectionSpecifiedError(String projectName)
+    {
         StringBuilder message = new StringBuilder();
         message.append("There is no connection type associated to <b>")
-                .append(projectName)
-                .append("</b> module.\nPlease right click on module name and then select <b>Module Connection Configuration</b> to select connection type!");
+            .append(projectName)
+            .append("</b> module.\nPlease right click on module name and then select <b>Module Connection Configuration</b> to select connection type!");
         Messages.showErrorDialog(message.toString(), "No connection specified for project " + projectName);
     }
 
@@ -64,52 +65,9 @@ public class Utils {
      * Extracts an ordered list of directories from project
      * root path to the file
      */
-    public static Path dirsToFileFromProjectRoot(VirtualFile virtualFile, Project project) {
-        String projectBaseDirUrl = project.getBaseDir().getUrl();
-        String relativeVirtualFilePath = virtualFile.getParent().getUrl().replace(projectBaseDirUrl, "");
-        relativeVirtualFilePath = relativeVirtualFilePath.replaceFirst("/", "");
-        return Paths.get(relativeVirtualFilePath);
-
-
-    }
-
-    /**
-     * Normalizes Windows paths to Unix path.
-     *
-     * @param path a Unix or Windows path.
-     * @return if a Unix like path.
-     */
-    public static String getUnixPath(String path) {
-        return path.replace("\\", "/");
-    }
-
-    /**
-     * Builds a Unix path from the list of specified strings. Array is taken in order.
-     *
-     * @param paths a list of relative string paths.
-     * @return a string instance representing a Unix path. It may or may not be an absolute path
-     * depending on the input array.
-     */
-    public static String buildUnixPath(String... paths) {
-        StringBuilder toReturn = new StringBuilder();
-        for (String path : paths) {
-            toReturn.append(getUnixPath(path)).append("/");
-        }
-        String finalValue = toReturn.toString().replace("//", "/");
-        if (finalValue.length() > 0 && finalValue.charAt(finalValue.length() - 1) == '/') {
-            return finalValue.substring(0, finalValue.length() - 1);
-        }
-        return finalValue;
-    }
-
-    /**
-     * Returns an array of folders that build up a file path
-     *
-     * @param path an absolute path.
-     * @return an array of strings instances that represent a file or folder path.
-     */
-    public static String[] splitPath(String path) {
-        return getUnixPath(path).split("/");
+    public static Path dirsToFileFromProjectRoot(VirtualFile virtualFile, IProjectStore projectStore)
+    {
+        return projectStore.getProjectBasePath().relativize(virtualFile.toNioPath());
     }
 
     /**
@@ -120,9 +78,11 @@ public class Utils {
      * be that the file already exists
      * @throws IOException if an I/O error occurred
      */
-    public static boolean createFile(String path) throws IOException {
+    public static boolean createFile(String path) throws IOException
+    {
         File fileToCreate = new File(path);
-        if (fileToCreate.exists()) {
+        if (fileToCreate.exists())
+        {
             return false;
         }
         // the file doesn't exist so try create it
