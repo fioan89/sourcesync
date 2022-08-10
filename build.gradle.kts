eab0d86964017236631f6270c6ca4b047c0ade01
Ioan Faur <fioan89@gmail.com>
2022-08-11 00:50:46 +0300

    Don't configure signPlugin task if we don't use it

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,12 +108,6 @@ tasks {
         systemProperty("jb.consents.confirmation.enabled", "false")
     }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
     publishPlugin {
         dependsOn("patchChangelog")
         token.set(System.getenv("PUBLISH_TOKEN"))
