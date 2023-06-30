5989b6d58da9b58acdb294d3ad5ca333cca98fb1
Faur Ioan-Aurel <fioan89@gmail.com>
2023-06-30 16:11:42 +0300

    build: downgrade again

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.serialization") version "1.8.22"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.14.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
